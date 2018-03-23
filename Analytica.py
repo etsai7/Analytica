91b70fb2ae0508c25298e00c41f5cce5f7a8b18b
etsai7 <e.tsai70@gmail.com>
2018-03-23 16:34:36 -0400

    Processed data into useable format

--- Analytica.py
+++ Analytica.py
@@ -1,13 +1,41 @@
-import sys
 import os.path
+from PreProcPDF import PreProcess
 import PyPDF2
-directory = os.fsencode("./Data")
+import codecs
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith(".pdf") or filename.endswith(".py"):
-        print(os.path.join(directory, file).decode("utf-8"))
-        # print(file)
-        continue
-    else:
-        continue
+directory = os.fsencode("./Data/PDF_Converted")
+
+def getDataDir(directory):
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".pdf"):
+
+            pdffilename = './Data/PDF_Converted/' + str(filename)
+            o = open(pdffilename, 'rb')
+            pdfFileReader = PyPDF2.PdfFileReader(o)
+            fname = './Data/Text/Pre/' + filename.rstrip("_pdf.pdf") + '_proc.txt'
+            file = filename.rstrip(".pdf") + '_proc.txt'
+
+            f = codecs.open(fname, 'w',"ISO-8859-1")
+
+            for x in range(0, 2): # Only need 1st 2 pages
+                text = pdfFileReader.getPage(x)
+                textp = text.extractText()
+                f.write(textp)
+            f.close()
+            fileSave = PreProcess(fname, file)
+
+            # Summary
+            print("File Name: " + filename)
+            print("\t%s has %d pages." % (filename, pdfFileReader.getNumPages()))
+            print("\tPDF File Dir     : " + pdffilename)
+            print("\tPreProc File Dir : " + fname)
+            print("\tPostProc File Dir: " + fileSave + "\n")
+
+
+        else:
+            continue
+
+
+
+getDataDir(directory)
