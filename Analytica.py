91c9a68f9b614506d6f16ff9d5eae47f5f6abd08
etsai7 <e.tsai70@gmail.com>
2018-03-23 18:36:25 -0400

    Parsed Yearly Total and works with Plotly

--- Analytica.py
+++ Analytica.py
@@ -1,8 +1,18 @@
 import os.path
 import PyPDF2
 import codecs
+import plotly.plotly as py
+from PIL import Image
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from re import sub
+from decimal import Decimal
 
 directory = os.fsencode("./Data/PDF_Converted")
+yearly_total = {}
+
 
 def getDataDir(directory):
     for file in os.listdir(directory):
@@ -30,6 +40,8 @@ def getDataDir(directory):
             print("\tPreProc File Dir : " + fname)
             print("\tPostProc File Dir: " + fileSave + "\n")
 
+            DataExtract(fileSave, file)
+
         else:
             continue
 
@@ -42,5 +54,47 @@ def PreProcess(filedir, file):
             outfile.write("\n")
     return fileSave
 
+def DataExtract(fileSave,fileName):
+    file = open(fileSave, 'r')
+    for line in file:
+        if "Total spent" in line:
+            year = fileName[0:4]
+            money = next(file).rstrip("\n")
+            total = Decimal(sub(r'[^\d.]', '', money))
+
+            print(line.rstrip("\n") + " " + str(total) + " in " + year + "\n")
+            yearly_total[year] = (total)
 
 getDataDir(directory)
+print(yearly_total)
+
+year = []
+val = []
+for key in yearly_total:
+    year.append(key)
+    val.append(yearly_total[key])
+
+print(year)
+print(val)
+# data = [go.Bar(
+#             x = year,
+#             y = val,
+#             text=val,
+#             textposition = 'auto',
+#             marker=dict(
+#                 color='rgb(158,202,225)',
+#                 line=dict(
+#                     color='rgb(8,48,107)',
+#                     width=1.5),
+#             ),
+#             opacity=0.6
+#     )]
+#
+# py.iplot(data, filename='Analytica-bar')
+
+fig = py.get_figure('https://plot.ly/~etsai7/2')
+py.image.save_as(fig,'etsai7.png')
+
+img = mpimg.imread('etsai7.png')
+plt.imshow(img)
+plt.show()
