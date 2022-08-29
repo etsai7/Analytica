8b4f605b11af0dfc8ebfad57fcb4090ce6e246f1
Eric Tsai <e.tsai70@gmail.com>
2022-08-29 16:35:04 -0400

    Filter by Column and cleaning data

--- src/excel_dash_plotter/Dashboard.py
+++ src/excel_dash_plotter/Dashboard.py
@@ -1,7 +1,19 @@
 import dashboard_secrets as sec
 import pandas as pd
 
-# 1. Use pandas to open up an excel sheet for reading
-df = pd.read_excel(f'{sec.expenses_default_dir}{sec.expenses_excel[0]}',sheet_name='Expenses')
 
-print(df.to_markdown())
+class Dashboard:
+    def __init__(self, df):
+        self.df = df
+        self.clean_data()
+
+    def clean_data(self):
+        self.df = self.df[["Date", "Type", "Company", "Net"]]  # Filter for specific columns
+        self.df = self.df.dropna()  # Remove Nans
+        print(self.df.Type.unique())  # Get unique Types
+        print(self.df.to_markdown())
+
+
+# Use pandas to open up an Excel sheet for reading
+dataframe = pd.read_excel(f'{sec.expenses_default_dir}{sec.expenses_excel[0]}', sheet_name='Expenses')
+dashboard = Dashboard(dataframe)
